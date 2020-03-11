8a2fee0c066b01f7f05b897caebf67a4b540d9b5
rayfungray <61134260+rayfungray@users.noreply.github.com>
2020-03-10 22:10:10 -0600

    starting project

---
+++ script.js
@@ -0,0 +1,41 @@
+var body = document.body;
+var addContainer = document.createElement('div');
+addContainer.classList.add('container');
+body.appendChild(addContainer);
+addButton(addContainer,'red','red');
+addButton(addContainer, 'blue', 'blue');
+addButton(addContainer, 'green', 'green');
+
+
+// var addColor = document.createElement('button');
+// addColor.
+
+var colorbtn = document.getElementsByClassName('color-btn');
+
+for (var i = 0; i < colorbtn.length; i++)
+{
+  colorbtn[i].addEventListener('click',function(event){
+    changeBKColor(body,event.target.value);
+  })
+}
+
+
+
+
+
+
+function addButton(container, color, colorHex){
+  var addBtn = document.createElement('button');
+  addBtn.classList.add('color-btn');
+  addBtn.innerHTML = color;
+  addBtn.style.background = colorHex;
+  addBtn.value = colorHex;
+  container.appendChild(addBtn);  
+}
+
+function changeBKColor(background, color)
+{
+  background.style.backgroundColor = color;
+
+}
+
